0f33a8f7981d18b5dba65b09ec240452ba06f71b
Lucio Ruixo <lucioruixo@gmail.com>
2020-08-20 23:21:22 -0300

    Noise map generation added

---
+++ Procedural Terrain/Assets/Editor/MapManagerEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapManager))]
+public class MapManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapManager mapManager = (MapManager)target;
+
+        if ((DrawDefaultInspector() && mapManager.autoUpdate) || GUILayout.Button("Generate")) mapManager.Generate();
+    }
+}
---
+++ Procedural Terrain/Assets/Scripts/MapManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public bool autoUpdate;
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octaves;
+    public float persistence;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public Renderer textureRenderer;
+
+    void Draw(float[,] noiseMap)
+    {
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        Color[] colorMap = new Color[width * height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+        }
+        texture.SetPixels(colorMap);
+        texture.Apply();
+
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(width, 1, height);
+    }
+
+    public void Generate()
+    {
+        float[,] noiseMap = PerlinNoise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
+
+        Draw(noiseMap);
+    }
+}
