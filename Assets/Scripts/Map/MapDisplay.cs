d8b57c39456ff8bb7c9cad62bb44e2a9d87a6d0c
LucioRuixo <54895404+LucioRuixo@users.noreply.github.com>
2021-08-05 16:59:31 -0300

    Mesh generation added (vertex mirroring error pending)

--- Assets/Scripts/Map/MapDisplay.cs
+++ Assets/Scripts/Map/MapDisplay.cs
@@ -2,29 +2,60 @@
 
 public class MapDisplay : MonoBehaviour
 {
-    public enum DisplayModes
+    public enum DrawModes
     {
         HeightMap,
-        ColorMap
+        ColorMap,
+        Mesh
     }
 
-    [SerializeField] DisplayModes displayMode = DisplayModes.HeightMap;
+    [SerializeField] DrawModes drawMode = DrawModes.HeightMap;
 
+    [Header("Map Objects")]
+    [SerializeField] GameObject mapPlaneObject = null;
+    [SerializeField] GameObject mapMeshObject = null;
+
+    [Header("Drawing Components")]
     [SerializeField] Renderer textureRenderer = null;
-    public MapGenerator mapGenerator = null;
+    [SerializeField] MeshRenderer meshRenderer = null;
+    [SerializeField] MeshFilter meshFilter = null;
 
     public bool autoUpdate = true;
+    public MapGenerator mapGenerator = null;
 
     public void Draw(float[,] noiseMap, Color[] colorMap)
     {
         int width = noiseMap.GetLength(0);
         int height = noiseMap.GetLength(1);
 
-        Texture2D texture;
-        if (displayMode == DisplayModes.ColorMap) texture = TextureGenerator.GenerateTextureFromColorMap(colorMap, width, height);
-        else texture = TextureGenerator.GenerateTextureFromHeightMap(noiseMap);
+        switch (drawMode)
+        {
+            case DrawModes.ColorMap:
+                textureRenderer.sharedMaterial.mainTexture = TextureGenerator.GenerateTextureFromColorMap(colorMap, width, height);
+                break;
 
-        textureRenderer.sharedMaterial.mainTexture = texture;
+            case DrawModes.Mesh:
+                meshFilter.sharedMesh = MeshGenerator.GenerateTerrainMesh(noiseMap).GenerateMesh();
+                meshRenderer.sharedMaterial.mainTexture = TextureGenerator.GenerateTextureFromColorMap(colorMap, width, height);
+                break;
+
+            case DrawModes.HeightMap:
+            default:
+                textureRenderer.sharedMaterial.mainTexture = TextureGenerator.GenerateTextureFromHeightMap(noiseMap);
+                break;
+        }
+
+        //if (drawMode == DrawModes.Mesh)
+        //{
+        //    mapPlaneObject.SetActive(false);
+        //    mapMeshObject.SetActive(true);
+        //}
+        //else
+        //{
+        //    mapMeshObject.SetActive(false);
+        //    mapPlaneObject.SetActive(true);
+        //}
+
         textureRenderer.transform.localScale = new Vector3(width, 1, height);
     }
 }
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public static class MeshGenerator
+{
+    public static MeshData GenerateTerrainMesh(float[,] heightMap)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+        float topLeftX = (width - 1) / -2.0f;
+        float topLeftY = (height - 1) / 2.0f;
+
+        MeshData meshData = new MeshData(width, height);
+
+        int vertexIndex = 0;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Debug.Log(meshData.vertices[y * width + x]);
+
+                meshData.vertices[y * width + x] = new Vector3(topLeftX + x, -heightMap[x, y], topLeftY + y);
+
+                if (x < width - 1 && y < height - 1)
+                {
+                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
+                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                }
+
+                meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
+
+                vertexIndex++;
+            }
+        }
+
+        return meshData;
+    }
+}
+
+public class MeshData
+{
+    public Vector3[] vertices;
+    public int[] triangles;
+    public Vector2[] uvs;
+
+    int triangleIndex = 0;
+
+    public MeshData(int meshWidth, int meshHeight)
+    {
+        vertices = new Vector3[meshWidth * meshHeight];
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+        uvs = new Vector2[meshWidth * meshHeight];
+    }
+
+    public void AddTriangle(int a, int b, int c)
+    {
+        triangles[triangleIndex++] = a;
+        triangles[triangleIndex++] = b;
+        triangles[triangleIndex++] = c;
+    }
+
+    public Mesh GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.SetUVs(0, uvs);
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+}
